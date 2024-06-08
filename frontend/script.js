590ea3241d5128da445eb0bdc1e7419deea40ba4
Ritesh Giri <rrgiri032@gmail.com>
2024-06-08 12:57:38 +0530

    Home page done.

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+let main = document.querySelector("main");
+let cursor = document.querySelector(".cursor");
+
+main.addEventListener("mousemove", function (dets) {
+  cursor.style.left = dets.x - 150 + "px";
+  cursor.style.top = dets.y - 150 + "px";
+});
+
+const left = document.querySelector(".links");
+
+let rect = left.getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
