8e16fd08a318b1cf0f92cf8703b616ceb3ea88ce
Ritesh Giri <rrgiri032@gmail.com>
2024-06-13 11:58:31 +0530

    Project section done

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-let main = document.querySelector("main");
+let main = document.querySelector("body");
 let cursor = document.querySelector(".cursor");
 
 main.addEventListener("mousemove", function (dets) {
@@ -6,8 +6,7 @@ main.addEventListener("mousemove", function (dets) {
   cursor.style.top = dets.y - 150 + "px";
 });
 
-const left = document.querySelector(".links");
+// const left = document.querySelector(".links");
 
-let rect = left.getBoundingClientRect();
-console.log(rect.top, rect.right, rect.bottom, rect.left);
-
+// let rect = left.getBoundingClientRect();
+// console.log(rect.top, rect.right, rect.bottom, rect.left);
