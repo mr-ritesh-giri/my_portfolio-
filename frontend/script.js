c1b483de4d356ee95a16cf7b2c2414d8a06ccf41
Ritesh Giri <rrgiri032@gmail.com>
2024-07-19 13:31:41 +0530

    Contact done

--- frontend/script.js
+++ frontend/script.js
@@ -5,8 +5,3 @@ document.addEventListener("mousemove", function (dets) {
   cursor.style.left = dets.pageX - 150 + "px";
   cursor.style.top = dets.pageY - 150 + "px";
 });
-
-// const left = document.querySelector(".links");
-
-// let rect = left.getBoundingClientRect();
-// console.log(rect.top, rect.right, rect.bottom, rect.left);
